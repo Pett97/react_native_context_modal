4d10094cdb706f45610c822f0280db2782fa799a
Pett97 <henriquepeterson138@gmail.com>
2025-02-05 21:06:41 -0300

    instalado expo rouer

---
+++ app/_layout.tsx
@@ -0,0 +1,8 @@
+import { Slot } from 'expo-router'
+import React from 'react'
+
+function _layout() {
+  return <Slot/>
+}
+
+export default _layout
---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+import React from 'react'
+
+function index() {
+  return (
+    <View><Text>Teste OK</Text></View>
+  )
+}
+
+export default index
