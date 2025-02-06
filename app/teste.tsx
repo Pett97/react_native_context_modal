0f3191e2a4e0f41f516d0afab63578a9a16ea341
Pett97 <henriquepeterson138@gmail.com>
2025-02-06 01:53:24 -0300

    instalado galio framework UI

--- app/teste.tsx
+++ app/teste.tsx
@@ -1,18 +1,45 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Card, Text, Button } from 'galio-framework';
+import { View, StyleSheet } from "react-native";
 import { useTheme } from "../src/contexts/ThemeContext";
-import globalStyle from "../styles/globalStyle";
 
 function Teste() {
   const { theme } = useTheme();
 
   return (
-    <View style={globalStyle(theme).container}>
-      <Text style={globalStyle(theme).title}>
-        Aqui eu devo ter "herdado" o tema
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hi, I'm a Galio component</Text>
+      <Button style={styles.button}>teste</Button>
+      <Card
+        flex
+        borderless
+        title="Christopher Moon"
+        caption="139 minutes ago"
+        location="Los Angeles, CA"
+        avatar="http://i.pravatar.cc/100?id=skater"
+        image="https://images.unsplash.com/photo-1497802176320-541c8e8de98d?&w=1600&h=900&fit=crop&crop=entropy&q=300"
+        style={styles.card}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    flex: 1,
+    justifyContent: 'center',
+  },
+  text: {
+    marginBottom: 10,
+    fontSize: 18,
+  },
+  button: {
+    marginVertical: 10,
+  },
+  card: {
+    marginTop: 20,
+  },
+});
 
 export default Teste;
